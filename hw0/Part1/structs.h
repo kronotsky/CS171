aae1b346d6e133889801a320eb60942d625a40e5
graham <gschmidt@alumni.caltech.edu>
2018-05-13 21:55:51 -0600

    hw0 Part 1 finished

--- hw0/Part1/structs.h
+++ hw0/Part1/structs.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
@@ -9,13 +10,39 @@ struct Vertex {
     float x, y, z;
 };
 
+#endif
+
+#ifndef __FACE__
+#define __FACE__
 struct Face {
     int a, b, c;
 };
 
+#endif
+
+#ifndef __OBJECT__
+#define __OBJECT__
+
 struct Object {
     vector<Vertex> vertices;
     vector<Face> faces;
+
+    Object() { };
+
+    void add_vertex(Vertex v) {
+	vertices.push_back(v);
+    }
+
+    void add_face(Face f) {
+	faces.push_back(f);
+    }
+
+    void print() const {
+	for (int i = 0; i < vertices.size(); i++) 
+	    cout << "v " << vertices[i].x << " " << vertices[i].y << " " << vertices[i].z << endl;
+	for (int i = 0; i < faces.size(); i++)
+	    cout << "f " << faces[i].a << " " << faces[i].b << " " << faces[i].c << endl;
+    }
 };
 
 #endif

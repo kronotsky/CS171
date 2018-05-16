a5442dab5f71ab92e335f9996752415f2fb75c83
graham <gschmidt@mail.alumni.caltech.edu>
2018-05-16 15:19:44 -0600

    Added new version of hw0 Part3 with updated Object struct

---
+++ hw0/Part3_2/structs.hh
@@ -0,0 +1,88 @@
+#include <vector>
+#include <iostream>
+#include <Eigen/Dense>
+
+using namespace std;
+
+#ifndef __VERTEX__
+#define __VERTEX__
+
+struct Vertex {
+    float x, y, z;
+};
+
+#endif
+
+#ifndef __FACE__
+#define __FACE__
+
+struct Face {
+    int a, b, c;
+};
+
+#endif
+
+
+#ifndef __OBJECT__
+#define __OBJECT__
+
+struct Object {
+    vector<Eigen::Vector4d> vertices;
+    vector<Face> faces;
+    
+    Object() { };
+    Object(vector<Eigen::Vector4d> vs, vector<Face> fs) : vertices(vs), faces(fs) { };
+    Object(const Object &source) : vertices(source.vertices), \
+                                   faces(source.faces) { };
+    
+    Object transform(Eigen::Matrix4d trans) const {
+	Object ret(*this);
+	ret *= trans;
+	return ret;
+    }
+    
+    Object & operator*=(Eigen::Matrix4d trans) {
+	for (auto &v : vertices) {
+	    v = trans * v;
+	}
+	return *this;
+    }
+
+    Object & operator=(const Object &rhs) {
+	if (&rhs == this) {
+	    return *this;
+	}
+	vertices = rhs.vertices;
+	faces = rhs.faces;
+	return *this;
+    }
+
+    vector<Vertex> cartesian_vertices() const {
+	vector<Vertex> ret;
+	for (auto &v : vertices) {
+	    Vertex vert;
+	    double w = v(3);
+	    vert.x = v(0) / w;
+	    vert.y = v(1) / w;
+	    vert.z = v(2) / w;
+	    ret.push_back(vert);
+	}
+	return ret;
+    }
+    
+    void print() const {
+	vector<Vertex> verts = this->cartesian_vertices();
+	for (int i = 0; i < verts.size(); i++) 
+	    cout << verts[i].x << " " << verts[i].y << " " << verts[i].z << endl;
+    }
+};
+
+
+#endif
+
+Eigen::Matrix4d translation(double tx, double ty, double tz);
+Eigen::Matrix4d scaling(double sx, double sy, double sz);
+Eigen::Matrix4d rotation(double rx, double ry, double rz, double phi);
+vector<string> split(string line, char delim = ' ');
+Object parse_obj(ifstream &objfile);
+Object transform(Object &obj, const Eigen::Matrix4d &trans);
---
+++ hw0/Part3_2/transform_objects.cpp
@@ -0,0 +1,77 @@
+#include "structs.hh"
+#include <fstream>
+#include <string>
+#include <string.h>
+#include <map>
+
+void parse_transform(ifstream &trfile) {
+    vector <string> tokens;
+    string line;
+    map<string, Object> objs;
+    map<string, int> copycount;
+    while (getline(trfile, line) && !line.empty()) {
+	tokens = split(line);
+	if (tokens.size() != 2) {
+	    cout << "Bad object file line!" << endl;
+	    continue;
+	}
+	ifstream objfile;
+	objfile.open(tokens[1]);
+	if (!(objfile.is_open())) {
+	    cout << "Filename " << tokens[1] << " not found!" << endl;
+	}
+	objs.insert(make_pair(tokens[0], parse_obj(objfile)));
+	copycount.insert(make_pair(tokens[0], 0));
+    }
+    while (getline(trfile, line)) {
+	tokens = split(line);
+	if (tokens.size() != 1) {
+	    cout << "Bad syntax line: '" << line << "'" << endl;
+	    continue;
+	}
+	else if (objs.find(line) == objs.end()) {
+	    cout << "Object '" << line << "' not found" << endl;
+	    continue;
+	}
+	string objname = line;
+	copycount[objname] += 1;
+	Eigen::Matrix4d cum = Eigen::MatrixXd::Identity(4,4);
+	while (getline(trfile, line) && !line.empty()) {
+	    tokens = split(line);
+	    if ((tokens.size() == 4) && (tokens[0] == "t")) {
+		cum *= translation(stod(tokens[1]), stod(tokens[2]),\
+				   stod(tokens[3]));
+	    }
+	    else if ((tokens.size() == 4) && (tokens[0] == "s")) {
+		cum *= scaling(stod(tokens[1]), stod(tokens[2]), \
+			       stod(tokens[3]));
+	    }
+	    else if ((tokens.size() == 5) && (tokens[0] == "r")) {
+		cum *= rotation(stod(tokens[1]), stod(tokens[2]), \
+				stod(tokens[3]), stod(tokens[4]));
+	    }
+	    else {
+		cout << "Bad line: '" << line << "', skipping." << endl;
+	    }
+	}
+	Object trans;
+	trans = objs[objname].transform(cum);
+	cout << objname << " copy #" << copycount[objname] << endl;
+	trans.print();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ifstream transfile;
+    if (argc != 2) {
+	cout << "Usage: " << argv[0] << " file " << endl;
+	return 1;
+    }
+    
+    transfile.open(argv[1]);
+    if (!(transfile.is_open())) {
+	cout << "Filename " << argv[1] << " not found!" << endl;
+	return 1;
+    }
+    parse_transform(transfile);
+}

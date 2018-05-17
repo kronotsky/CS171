75f590f4cce4c67f69b9b3541c18f6a448983084
graham <gschmidt@mail.alumni.caltech.edu>
2018-05-17 12:00:02 -0600

    Added Pixel/Grid objects, and wrote untested version of rasterize function

--- hw1/transform_objects.cpp
+++ hw1/transform_objects.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <string>
 #include <string.h>
+#include <stdlib.h>
 #include <map>
+
 
 Eigen::Matrix4d parse_transformations(ifstream &trfile) {
     vector<string> tokens;
@@ -132,9 +134,67 @@ vector<Object> parse_scene(ifstream &trfile) {
     return torender;
 }
 
-void rasterize(Pixel a, Pixel b, int *grid, int xres, int yres);
+void setpixel(int x, int y, int *grid, int xres, int val) {
+    grid[x + y * xres] = val;
+}
 
-void render(Object obj, int *grid, int xres, int yres) {
+void rasterize(Pixel a, Pixel b, Grid im) {
+    // Get dx and dy:
+    int dx = a.x - b.x;
+    int dy = a.y - b.y;
+
+    // Obtain the signs:
+    int sx = (dy >= 0) ? 1 : -1;
+    int sy = (dx >= 0) ? 1 : -1;
+
+    // bool xlong is true iff abs(dx) >= abs(dy)
+    bool xlong;
+
+    // dl and ds serve as "dx and dy" from the first-octant
+    // algorithm:
+    int dl, ds;
+
+    // Set dl to be the longer dimension length, ds the shorter,
+    // determine xlong:
+    if (dx * sx >= dy * sy) {
+	dl = dx * sx;
+	ds = dy * sy;
+	xlong = true;
+    }
+    else {
+	dl = dy * sy;
+	ds = dx * sy;
+	xlong = false;
+    }
+
+    // Procede with the algorithm as normal, using l, s instead
+    // of x, y:
+    int eps = 0;
+    int s = 0;
+    int l;
+    for (l = 0; l < dl; l++) {
+
+	// This is where the generalization happens: map l, s onto
+	// +/- x, +/- y depending on sx and xlong, and set that
+	// pixel:
+	if (xlong) 
+	    im.set(a.x + l * sx, a.y + s * sy, 255);
+	else
+	    im.set(a.x + s * sx, a.y + l * sy, 255);
+
+	// Rest of algorithm as normal:
+	if (((eps + ds) << 1) < dl) {
+	    eps += ds;
+	}
+	else {
+	    eps += ds - dl;
+	    s += 1;
+	}
+    }
+    
+}
+
+void render(Object obj, Grid im, int xres, int yres) {
     vector<Pixel> pixels;
     vector<Face> faces = obj.faces;
     vector<Vertex> vertices = obj.cartesian_vertices();
@@ -156,11 +216,15 @@ void render(Object obj, int *grid, int xres, int yres) {
 	c = f.c - 1;
 	if (exclude[a] || exclude[b] || exclude[c])
 	    continue;
-	rasterize(pixels[a], pixels[b], grid, xres, yres);
-	rasterize(pixels[b], pixels[c], grid, xres, yres);
-	rasterize(pixels[c], pixels[a], grid, xres, yres);
+	rasterize(pixels[a], pixels[b], im, xres, yres);
+	rasterize(pixels[b], pixels[c], im, xres, yres);
+	rasterize(pixels[c], pixels[a], im, xres, yres);
     }
     delete[] exclude;
+}
+
+void grid_to_ppm(Grid im) {
+    // To do: test rasterization using this method
 }
 
 int main(int argc, char *argv[]) {
@@ -179,5 +243,7 @@ int main(int argc, char *argv[]) {
 	return 1;
     }
     vector<Object> torender = parse_scene(transfile);
-    int *grid = new int[xres * yres];
+    Grid im(xres, yres);
 }
+
+

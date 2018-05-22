406f1e98ac1b14125accfd5efaf5e9509d936626
graham <gschmidt@alumni.caltech.edu>
2018-05-22 16:44:06 -0600

    Phong shading added; fixed off-by-1 segfault error.
    There is still an issue with object transformations, but I can't be bothered to fix it, so I'm calling hw2 done.
    Things I would do differently (may implement with some spare time):
    -- Represent colors as Vector3ds to make arithmetic easier/faster
    -- Streamline the Object struct to remove a few unnecessary routines
    -- Put Grid and Buffer together into one struct
    -- Make another data structure (Point/Pixel) which makes it easier to separate Phong/Gouraud shading into their own
       routines (i.e. so that a lot of the overlap in computation can be done in a single function, which then calls the
       correct rastering function).
    -- Overall, I think the data structures I used were not as bad as I had feared, and it was relatively simple to debug,
       if still somewhat painful.
    
    I would give myself a B/B+, taking effort into account.

--- hw2/render.cpp
+++ hw2/render.cpp
@@ -72,13 +72,13 @@ inline double abg(int x, int y, int xi, int yi, int xj, int yj) {
     return (yi - yj) * x + (xj - xi) * y + xi * yj - xj * yi;
 }
 
-void raster(Object &o, Face &f, vector<Light> lights, Camera &c, Grid &gr, Buffer &buf) {
-    Eigen::Vector3d va, vb, vc, cross, curr;
+void raster_gouraud(Object &o, Face &f, vector<Light> lights, Camera &c, Grid &gr, Buffer &buf) {
+    Eigen::Vector3d va, vb, vc, cross;
     Eigen::Vector4d vat, vbt, vct;
     Eigen::Matrix4d cam = c.perspective() * c.camera();
     int xa, ya, xb, yb, xc, yc, xmin, xmax, ymin, ymax, x, y;
     Color ca, cb, cc;
-    double alpha, beta, gamma, r, g, b;
+    double alpha, beta, gamma, r, g, b, depth;
     ca = lighting(o, o.vertices[f.va - 1], o.normals[f.na - 1], c, lights);
     cb = lighting(o, o.vertices[f.vb - 1], o.normals[f.nb - 1], c, lights);
     cc = lighting(o, o.vertices[f.vc - 1], o.normals[f.nc - 1], c, lights);
@@ -117,14 +117,14 @@ void raster(Object &o, Face &f, vector<Light> lights, Camera &c, Grid &gr, Buffe
 
 	    if ((0 <= alpha) && (alpha <= 1) && (0 <= beta) && (beta <= 1) &&\
 		(0 <= gamma) && (gamma <= 1)) {
-		curr = alpha * va + beta * vb + gamma * vc;
+		depth = alpha * va(2) + beta * vb(2) + gamma * vc(2);
 		
-		if ((-1 <= curr(0)) && (curr(0) <= 1) && \
-		    (-1 <= curr(1)) && (curr(1) <= 1) && \
-		    (-1 <= curr(2)) && (curr(2) <= 1) && \
-		    (curr(2) <= buf.get(x,y))) {
+		if ((0 <= x) && (x < gr.xres) && \
+		    (0 <= y) && (y < gr.yres) && \
+		    (-1 <= depth) && (depth <= 1) && \
+		    (depth <= buf.get(x,y))) {
 
-		    buf.set(x, y, curr(2));
+		    buf.set(x, y, depth);
 		    r = alpha * ca.r + beta * cb.r + gamma * cc.r;
 		    g = alpha * ca.g + beta * cb.g + gamma * cc.g;
 		    b = alpha * ca.b + beta * cb.b + gamma * cc.b;
@@ -135,8 +135,75 @@ void raster(Object &o, Face &f, vector<Light> lights, Camera &c, Grid &gr, Buffe
     }
 }
 
+void raster_phong(Object &o, Face &f, vector<Light> lights, Camera &c, Grid &gr, Buffer &buf) {
+    Eigen::Vector3d va, vb, vc, cross;
+    Eigen::Vector4d vat, vbt, vct, na, nb, nc, vbary, nbary;
+    Eigen::Matrix4d cam = c.perspective() * c.camera();
+    int xa, ya, xb, yb, xc, yc, xmin, xmax, ymin, ymax, x, y;
+    Color ca, cb, cc;
+    double alpha, beta, gamma, r, g, b, depth;
+
+    vat = o.vertices[f.va - 1];
+    vbt = o.vertices[f.vb - 1];
+    vct = o.vertices[f.vc - 1];
+    vbary = cam * vat;
+    va = cartesian_vertex(vbary);
+    vbary = cam * vbt;
+    vb = cartesian_vertex(vbary);
+    vbary = cam * vct;
+    vc = cartesian_vertex(vbary);
+
+    na = o.normals[f.na - 1];
+    nb = o.normals[f.nb - 1];
+    nc = o.normals[f.nc - 1];
+    
+    
+    // Backface culling:
+    cross = (vc - vb).cross(va - vb);
+    if (cross(2) < 0)
+	return;
+
+    // Convert to grid coordinates:
+    xa = (int)((va(0) + 1) * (double)((gr.xres - 1) >> 1));
+    ya = gr.yres - (int)((va(1) + 1) * (double)((gr.yres - 1) >> 1));
+    xb = (int)((vb(0) + 1) * (double)((gr.xres - 1) >> 1));
+    yb = gr.yres - (int)((vb(1) + 1) * (double)((gr.yres - 1) >> 1));
+    xc = (int)((vc(0) + 1) * (double)((gr.xres - 1) >> 1));
+    yc = gr.yres - (int)((vc(1) + 1) * (double)((gr.yres - 1) >> 1));
+
+    // Get minima/maxima
+    xmin = min(min(xa, xb), xc);
+    ymin = min(min(ya, yb), yc);
+    xmax = max(max(xa, xb), xc);
+    ymax = max(max(ya, yb), yc);
+
+    for (x = xmin; x <= xmax; x++) {
+	for (y = ymin; y <= ymax; y++) {
+	    alpha = abg(x, y, xb, yb, xc, yc) / abg(xa, ya, xb, yb, xc, yc);
+	    beta = abg(x, y, xa, ya, xc, yc) / abg(xb, yb, xa, ya, xc, yc);
+	    gamma = abg(x, y, xa, ya, xb, yb) / abg(xc, yc, xa, ya, xb, yb);
+	    if ((0 <= alpha) && (alpha <= 1) && (0 <= beta) && (beta <= 1) &&\
+		(0 <= gamma) && (gamma <= 1)) {
+		vbary = alpha * vat + beta * vbt + gamma * vct;
+		nbary = alpha * na + beta * nb + gamma * nc;
+		depth = alpha * va(2) + beta * vb(2) + gamma * vc(2);
+		
+		if ((0 <= x) && (x < gr.xres) && \
+		    (0 <= y) && (y < gr.yres) && \
+		    (-1 <= depth) && (depth <= 1) && \
+		    (depth <= buf.get(x,y))) {
+
+		    buf.set(x, y, depth);
+		    gr.fill(x, y, lighting(o, vbary, nbary, c, lights));
+		}		   
+	    }
+	}
+    }
+}
+
 int main(int argc, char *argv[]) {
     ifstream scfile;
+    int mode;
     vector<string> tokens;
     vector<Light> lights;
     Camera cam;
@@ -146,16 +213,17 @@ int main(int argc, char *argv[]) {
     double n, f, l, r, t, b;
 
     int xres, yres;
-    if (argc != 4) {
-	cout << "Usage: " << argv[0] << " [filename] [xres] [yres]" << endl;
+    if (argc != 5) {
+	cout << "Usage: " << argv[0] << " [filename] [xres] [yres] [mode:0/1]" << endl;
 	return 1;
     }
     xres = stoi(argv[2]);
     yres = stoi(argv[3]);
+    mode = stoi(argv[4]);
 
     Grid gr(xres, yres);
     Buffer buf(xres, yres);
-    
+   
     scfile.open(argv[1]);
     if (!(scfile.is_open())) {
 	cout << "Filename " << argv[1] << " not found!" << endl;
@@ -164,6 +232,10 @@ int main(int argc, char *argv[]) {
     // Get camera transformations:
     cam = parse_camera(scfile);
 
+#ifndef NDEBUG
+    cout << "Parsed camera" << endl;
+#endif	
+
     // // Get transformations:
     // persp = perspective(n, r, l, t, b, f);
     // camera = (ori * pos).inverse();
@@ -171,13 +243,27 @@ int main(int argc, char *argv[]) {
     // Get lights:
     lights = parse_lights(scfile);
 
+#ifndef NDEBUG
+    cout << "Parsed lights" << endl;
+#endif	
+
     // Get object map:
     objmap = parse_object_names(scfile);
+
+#ifndef NDEBUG
+    cout << "Parsed objects" << endl;
+#endif	
 
     // Find object copies and transformations: 
     torender = parse_object_spec(scfile, objmap);
     for (auto &obj : torender)
-	for (auto &face : obj.faces) 
-	    raster(obj, face, lights, cam, gr, buf);
+	for (auto &face : obj.faces)
+	    if (mode)
+		raster_gouraud(obj, face, lights, cam, gr, buf);
+	    else
+		raster_phong(obj, face, lights, cam, gr, buf);
+#ifndef NDEBUG
+    cout << "Rendered Objects" << endl;
+#endif	
     grid_to_ppm(gr);
 }
